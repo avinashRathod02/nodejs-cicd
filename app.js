211c4ce4b5c58851aad866d4a707e111f36654d9
avinashrathod02 <avinashrathod506@gmail.com>
2023-08-15 00:03:06 +0530

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+// Import the built-in 'http' module
+const http = require("http");
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, Node.js!");
+});
+
+// Listen on port 3000
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
